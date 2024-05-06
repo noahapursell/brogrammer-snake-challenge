103c98baa6869e9321ad6b438a86073c1872f740
noahapursell <noahapursell@gmail.com>
2024-05-06 10:29:57 -0500

    added a command line player

--- players/cli_player.py
+++ players/cli_player.py
@@ -1,6 +1,7 @@
 import numpy as np
 from lib.models.snake_player import SnakePlayer
-
+from lib.models.game_action import GameAction
+from lib.models.game_state import GameState
 class CLIPlayer(SnakePlayer):
     """Command-line-interface player for the snake game"""
 
@@ -8,15 +9,26 @@ class CLIPlayer(SnakePlayer):
         pass
 
     def get_action(self, state):
+        if state.died:
+            print("Game Over")
+            return
         user_input = input("Please enter a move (l, r, u, d): ")
 
         input_to_output = {
-                "u": np.array([1, 0, 0, 0]),
-                "r": np.array([0, 1, 0, 0]),
-                "d": np.array([0, 0, 1, 0]),
-                "l": np.array([0, 0, 0, 1])
+            "l": GameAction.LEFT,
+            "r": GameAction.RIGHT,
+            "u": GameAction.UP,
+            "d": GameAction.DOWN
         }
 
         output = input_to_output[user_input]
         print(f"User entered: {output}")
         return output
+
+    def notify_end_of_game(self, game_state: GameState) -> None:
+        print("Game over")
+        return
+
+    def notify_win(self):
+        print("Game beat!")
+        return
